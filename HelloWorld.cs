8f195e5885c31ff7432d34113e06b673f2452d55
jmiddendo <jeffrey.middendorf@gmail.com>
2017-01-13 13:19:40 +1100

    Hello World in C#

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadLine();
+        }
+    }
+}
